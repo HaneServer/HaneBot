2374aa78e27f230e5edeb1165c683d299e778599
Massa-san <masaharasan@gmail.com>
2022-09-16 18:32:10 +0900

    [Update] Change token management

--- src/main/kotlin/hane/server-on/hane/HaneBot.kt
+++ src/main/kotlin/hane/server-on/hane/HaneBot.kt
@@ -1,16 +1,22 @@
 package hane.`server-on`.hane
 
+import com.google.gson.Gson
 import com.jagrosh.jdautilities.command.CommandClientBuilder
+import io.github.cdimascio.dotenv.Dotenv
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.requests.GatewayIntent
+import java.io.File
+import java.io.InputStreamReader
+import java.io.ObjectInputFilter
 
 class HaneBot {
     private lateinit var jda: JDA
 
+    val env = Dotenv.configure().load()
     fun create(token: String) {
         val commandClient = CommandClientBuilder()
-            .setPrefix("h.")
+            .setPrefix(env.get("PREFIX"))
             .setOwnerId("752090084639244408")
             .addCommands()
             .build()
@@ -27,5 +33,6 @@ class HaneBot {
 
 fun main() {
     val bot = HaneBot()
-    bot.create("")
+    val env = Dotenv.load()
+    bot.create(env.get("TOKEN"))
 }
